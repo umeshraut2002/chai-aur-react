7457ac33ac291bcb8e4b95f3855755121351339f
umesh raut <uraut6044@gmail.com>
2024-11-11 10:04:55 +0530

    todo form is updated

--- 10todocontextLocal/src/components/TodoForm.jsx
+++ 10todocontextLocal/src/components/TodoForm.jsx
@@ -1,3 +1,22 @@
+import React, { useState } from 'react';
+import { useTodo } from '../contexts/TodoContext';
+
+const TodoForm = () => {
+
+    const [todo, setTodo] = useState("");
+
+    const {addTodo} = useTodo()
+
+    const add = (e) => {
+        e.preventDefault();
+
+        if(!todo)  return;
+        
+        addTodo({todo, completed: false})
+
+        setTodo("");
+
+    }
 
   return (
       <form onSubmit={add}  className="flex">
--- 10todocontextLocal/src/components/TodoItem.jsx
+++ 10todocontextLocal/src/components/TodoItem.jsx
@@ -1,4 +1,6 @@
+import React from "react";
 
+const TodoItem = () => {
   return (
       <div
           className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
